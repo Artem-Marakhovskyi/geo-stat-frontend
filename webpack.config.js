4c6337421cd77ceb033a0f5fb382d3aa18ac84c9
Alex <sanekwwe@yandex.ua>
2018-12-27 13:38:45 +0200

    webpack.config fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
-var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+//var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 module.exports = {
     entry: {
         'polyfills': './src/polyfills.ts',
@@ -33,7 +33,7 @@ module.exports = {
             /angular(\\|\/)core/,
             path.resolve(__dirname, 'src'),
             {}
-        ),
-        new UglifyJSPlugin()
+        )
+        //new UglifyJSPlugin()
     ]
 }
